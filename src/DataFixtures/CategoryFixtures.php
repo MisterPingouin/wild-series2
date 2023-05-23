51d7631a086ae26614a9f0538692655ec2ce4edf
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-05-23 10:09:11 +0200

    Quest Symfony 09 success

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -8,16 +8,21 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
+    public const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+    ];
     public function load(ObjectManager $manager)
     {
-        $categories = ['Horreur', 'Action', 'Comédie', 'Drame', 'Science-fiction', 'Thriller', 'Fantaisie', 'Documentaire', 'Animation', 'Aventure', 'Mystère', 'Romance', 'Policier', 'Historique', 'Guerre', 'Super-héros', 'Western', 'Musical', 'Sport', 'Biographie', 'Politique', 'Cuisine', 'Jeunesse', 'Espionnage', 'Fantastique', 'Sitcom', 'Reality show', 'Téléréalité', 'Game show', 'Talk show', 'Variétés', 'Anthologie', 'Émission de voyage', 'Téléfilm', 'Soap opera', 'Art', 'Nature', 'Technologie', 'Éducation', 'Humour', 'Fashion', 'Web-série', 'Horreur', 'Action', 'Comédie', 'Drame', 'Science-fiction', 'Thriller', 'Fantaisie', 'Documentaire', 'Animation'];
-
-        foreach ($categories as $categoryName) {
+        foreach (self::CATEGORIES as $categoryName) {
             $category = new Category();
             $category->setName($categoryName);
             $manager->persist($category);
+            $this->addReference('category_' . $categoryName, $category);
         }
-
         $manager->flush();
     }
 }
---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const PROGRAMS = [
+        [
+            'title' => 'John Wick',
+            'synopsis' => 'Pan pan pan Bing Bing',
+            'category' => 'Action',
+        ],
+        [
+            'title' => 'Paddington',
+            'synopsis' => 'Un ours maléfique',
+            'category' => 'Aventure',
+        ],
+        [
+            'title' => 'Babar',
+            'synopsis' => 'Un éléphant ça trompe énormément',
+            'category' => 'Animation',
+        ],
+        [
+            'title' => 'Cleopatra',
+            'synopsis' => 'Un faux docu',
+            'category' => 'Fantastique',
+        ],
+        [
+            'title' => 'Bambi',
+            'synopsis' => 'Une biche tueuse',
+            'category' => 'Horreur',
+        ],
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::PROGRAMS as $programInfo) {
+            $program = new Program();
+            $program->setTitle($programInfo['title']);
+            $program->setSynopsis($programInfo['synopsis']);
+            $category = $this->getReference('category_' . $programInfo['category']);
+            $program->setCategory($category);
+            $manager->persist($program);
+        }
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+}
